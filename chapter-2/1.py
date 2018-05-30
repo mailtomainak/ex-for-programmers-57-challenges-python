32a9bbfdbba79687fd1edcf3064a501b42673c28
mailtomainak <mailtomainak@gmail.com>
2018-05-30 22:33:59 +0530

    Initial Check In

---
+++ chapter-2/1.py
@@ -0,0 +1,9 @@
+#Hello World
+print("What is your name?")
+name = input();
+print("Hello ,"+name+" nice to meet you!")
+
+#challenge #1
+print("What is your name?")
+#name = input();
+print("Hello ,"+input()+" nice to meet you!")
---
+++ chapter-2/2.py
@@ -0,0 +1,12 @@
+#Counting the Number of Characters
+print("what is the input string?");
+inputString = input();
+print(inputString+" has "+str(len(inputString))+" characters")
+
+#challenges
+print("what is the input string?");
+inputString = input();
+if len(inputString) == 0:
+    print("Input must be entered");
+else:
+    print(inputString+" has "+str(len(inputString))+" characters")
